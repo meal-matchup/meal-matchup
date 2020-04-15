00c52d83bc138ed8c887cb97b6685907981c857a
Griffen Schwiesow <schwigri@pm.me>
2020-04-15 13:11:43 -0700

    :lipstick: Update layout for auth pages

--- src/components/Auth/index.js
+++ src/components/Auth/index.js
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import firebase from 'gatsby-plugin-firebase';
 import { AnimatePresence } from 'framer-motion';
 import debug from 'debug';
 
-import { Button, Divider, Form, Input, Checkbox } from 'antd';
+import { Button, Divider, Form, Input, Checkbox, Select, Popover } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
 import AuthScreen from './AuthScreen';
+import SEO from '../SEO';
 
 function Auth() {
 	const [activePage, setActivePage] = useState('welcome');
@@ -22,23 +23,59 @@ function Auth() {
 	const [signUpPasswordStatus, setSignUpPasswordStatus] = useState(null);
 	const [signUpConfimStatus, setSignUpConfirmStatus] = useState(null);
 
-	const goToPage = page => setActivePage(page);
+	const [umbrellas, setUmbrellas] = useState([]);
 
-	const logIn = values => {
+	useEffect(() => {
+		if (!firebase || umbrellas.length > 0) return;
+
+		let status = true;
+
+		const getUmbrellas = async () => {
+			if (status)
+				return firebase
+					.firestore()
+					.collection('umbrellas')
+					.get()
+					.then((querySnapshot) => {
+						const allUmbrellas = [];
+						querySnapshot.forEach((doc) => {
+							allUmbrellas.push({
+								id: doc.id,
+								name: doc.data().name,
+							});
+						});
+						if (status && umbrellas) setUmbrellas(allUmbrellas);
+					})
+					.catch((e) => {
+						debug('Failed to get umbrellas', e);
+					});
+		};
+		getUmbrellas();
+
+		return () => {
+			status = false;
+		};
+	}, [umbrellas]);
+
+	const goToPage = (page) => setActivePage(page);
+
+	const logIn = (values) => {
 		if (!firebase) return false;
 		setLoggingIn(true);
-		setLogInStatus('validating')
+		setLogInStatus('validating');
 		const { email, password } = values.login;
 
-		return firebase.auth().signInWithEmailAndPassword(email, password)
-			.catch(e => {
+		return firebase
+			.auth()
+			.signInWithEmailAndPassword(email, password)
+			.catch((e) => {
 				debug('Log in error', e);
 				setLoggingIn(false);
 				setLogInStatus('error');
 			});
 	};
 
-	const signUp = values => {
+	const signUp = (values) => {
 		if (!firebase) return false;
 
 		setSigningUp(true);
@@ -46,7 +83,7 @@ function Auth() {
 		setSignUpPasswordStatus('validating');
 		setSignUpConfirmStatus('validating');
 
-		const { email, password, confirm } = values.signup;
+		const { name, email, password, confirm, umbrella } = values.signup;
 		if (confirm !== password) {
 			setSigningUp(false);
 			setSignUpEmailStatus(null);
@@ -55,13 +92,26 @@ function Auth() {
 			return false;
 		}
 
-		return firebase.auth().createUserWithEmailAndPassword(email, password)
+		return firebase
+			.auth()
+			.createUserWithEmailAndPassword(email, password)
 			.then((result) => {
-				return firebase.firestore().collection('users').doc(result.user.uid).set({
-					email: result.user.email,
-				});
+				return result.user
+					.updateProfile({
+						displayName: name,
+					})
+					.then(() => {
+						return firebase
+							.firestore()
+							.collection('users')
+							.doc(result.user.uid)
+							.set({
+								email: result.user.email,
+								umbrella: umbrella,
+							});
+					});
 			})
-			.catch(e => {
+			.catch((e) => {
 				debug(e);
 				setSigningUp(false);
 				setSignUpEmailStatus('error');
@@ -69,15 +119,19 @@ function Auth() {
 				setSignUpConfirmStatus('error');
 				return false;
 			});
-	}
+	};
 
 	return (
 		<div className="auth-screens">
+			<SEO title="Welcome to Meal Matchup" />
+
 			<AnimatePresence>
 				{activePage === 'welcome' && (
 					<AuthScreen id="welcome">
 						<p>Welcome</p>
-						<Button type="link" onClick={() => goToPage('log-in')}>Log In</Button>
+						<Button type="link" onClick={() => goToPage('log-in')}>
+							Log In
+						</Button>
 					</AuthScreen>
 				)}
 
@@ -99,7 +153,11 @@ function Auth() {
 								hasFeedback
 								validateStatus={logInStatus}
 							>
-								<Input disabled={loggingIn} prefix={<UserOutlined />} type="email" />
+								<Input
+									disabled={loggingIn}
+									prefix={<UserOutlined />}
+									type="email"
+								/>
 							</Form.Item>
 
 							<Form.Item
@@ -114,19 +172,32 @@ function Auth() {
 								hasFeedback
 								validateStatus={logInStatus}
 							>
-								<Input disabled={loggingIn} prefix={<LockOutlined />} type="password" />
+								<Input
+									disabled={loggingIn}
+									prefix={<LockOutlined />}
+									type="password"
+								/>
 							</Form.Item>
 
 							<Form.Item>
-								<Button disabled={loggingIn} type="primary" htmlType="submit" block>Log In</Button>
+								<Button
+									disabled={loggingIn}
+									type="primary"
+									htmlType="submit"
+									block
+								>
+									Log In
+								</Button>
 							</Form.Item>
 						</Form>
 
 						<Divider />
 
 						<p>
-							Don't have an account?
-							<Button type="link" onClick={() => goToPage('sign-up')}>Sign up now!</Button>
+							Don’t have an account?
+							<Button type="link" onClick={() => goToPage('sign-up')}>
+								Sign up now!
+							</Button>
 						</p>
 					</AuthScreen>
 				)}
@@ -136,6 +207,21 @@ function Auth() {
 						<h1>Sign Up</h1>
 
 						<Form layout="vertical" form={signUpForm} onFinish={signUp}>
+							<Form.Item
+								name={['signup', 'name']}
+								label="Name"
+								rules={[
+									{
+										required: true,
+										message: 'Please enter your name',
+									},
+								]}
+								hasFeedback
+								validateStatus={signUpEmailStatus}
+							>
+								<Input disabled={signingUp} type="text" />
+							</Form.Item>
+
 							<Form.Item
 								name={['signup', 'email']}
 								label="Email address"
@@ -164,7 +250,11 @@ function Auth() {
 								hasFeedback
 								validateStatus={signUpPasswordStatus}
 							>
-								<Input disabled={signingUp} onChange={e => setSignUpPassword(e.target.value)} type="password" />
+								<Input
+									disabled={signingUp}
+									onChange={(e) => setSignUpPassword(e.target.value)}
+									type="password"
+								/>
 							</Form.Item>
 
 							<Form.Item
@@ -176,7 +266,12 @@ function Auth() {
 										message: 'Please confirm your password',
 									},
 									{
-										validator: (_, value) => value ? signUpPassword === value ? Promise.resolve() : Promise.reject('Your passwords do not match') : Promise.resolve(),
+										validator: (_, value) =>
+											value
+												? signUpPassword === value
+													? Promise.resolve()
+													: Promise.reject('Your passwords do not match')
+												: Promise.resolve(),
 									},
 								]}
 								hasFeedback
@@ -185,20 +280,56 @@ function Auth() {
 								<Input disabled={signingUp} type="password" />
 							</Form.Item>
 
+							<Form.Item label="Organization">
+								<Form.Item
+									name={['signup', 'umbrella']}
+									rules={[
+										{
+											required: true,
+											message: 'Please select your organization',
+										},
+									]}
+								>
+									<Select>
+										{umbrellas.map((umbrella) => (
+											<Select.Option key={umbrella.id} value={umbrella.id}>
+												{umbrella.name}
+											</Select.Option>
+										))}
+									</Select>
+								</Form.Item>
+
+								<Popover content="Your organization might not be on Meal Matchup yet! Email join@mealmatchup.org">
+									<a href="#orgs">Don’t see your organization?</a>
+								</Popover>
+							</Form.Item>
+
 							<Form.Item
 								name={['signup', 'tos']}
 								valuePropName="checked"
 								rules={[
 									{
-										validator:(_, value) => value ? Promise.resolve() : Promise.reject('Should accept agreement'),
+										validator: (_, value) =>
+											value
+												? Promise.resolve()
+												: Promise.reject('Should accept agreement'),
 									},
 								]}
 							>
-								<Checkbox disabled={signingUp}>I agree to the terms of service</Checkbox>
+								<Checkbox disabled={signingUp}>
+									I agree to the terms of service
+								</Checkbox>
 							</Form.Item>
 
 							<Form.Item>
-								<Button disabled={signingUp} type="primary" htmlType="submit" block>Sign Up</Button>
+								<Button
+									disabled={signingUp}
+									type="primary"
+									htmlType="submit"
+									block
+								>
+									Sign Up
+								</Button>
 							</Form.Item>
 						</Form>
 
@@ -206,7 +337,9 @@ function Auth() {
 
 						<p>
 							Already have an account?
-							<Button type="link" onClick={() => goToPage('log-in')}>Log in</Button>
+							<Button type="link" onClick={() => goToPage('log-in')}>
+								Log in
+							</Button>
 						</p>
 					</AuthScreen>
 				)}
