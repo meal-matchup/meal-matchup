1da0825b5679b1ec928ddb795a295d63e75cdb8f
Griffen Schwiesow <schwigri@pm.me>
2020-04-21 17:07:05 -0700

    :construction: Update Account view prop requirements

--- src/components/App/Views/AccountView.js
+++ src/components/App/Views/AccountView.js
@@ -14,37 +14,41 @@ function AccountView({ user, umbrella, agency }) {
 
 	return (
 		<>
-			<Descriptions column={1} bordered>
-				<Descriptions.Item label="Name">{user.displayName}</Descriptions.Item>
-				<Descriptions.Item label="Email address">
-					{user.email}
-					{!!user.emailVerified && (
-						<CheckCircleTwoTone
-							twoToneColor="#52c41a"
-							style={{ marginLeft: '.5em' }}
-						/>
-					)}
-				</Descriptions.Item>
-				<Descriptions.Item label="Umbrella">{umbrella.name}</Descriptions.Item>
-				<Descriptions.Item label="Agency">
-					{agency.name}
-					{agency.approved ? (
-						<Tooltip title="Your agency has been approved">
+			{user && umbrella && agency && (
+				<Descriptions column={1} bordered>
+					<Descriptions.Item label="Name">{user.displayName}</Descriptions.Item>
+					<Descriptions.Item label="Email address">
+						{user.email}
+						{!!user.emailVerified && (
 							<CheckCircleTwoTone
 								twoToneColor="#52c41a"
 								style={{ marginLeft: '.5em' }}
 							/>
-						</Tooltip>
-					) : (
-						<Tooltip title="Your agency is pending approval">
-							<ClockCircleTwoTone style={{ marginLeft: '.5em' }} />
-						</Tooltip>
-					)}
-				</Descriptions.Item>
-				<Descriptions.Item label="Agency type">
-					{agencyTypes[agency.type]}
-				</Descriptions.Item>
-			</Descriptions>
+						)}
+					</Descriptions.Item>
+					<Descriptions.Item label="Umbrella">
+						{umbrella.name}
+					</Descriptions.Item>
+					<Descriptions.Item label="Agency">
+						{agency.name}
+						{agency.approved ? (
+							<Tooltip title="Your agency has been approved">
+								<CheckCircleTwoTone
+									twoToneColor="#52c41a"
+									style={{ marginLeft: '.5em' }}
+								/>
+							</Tooltip>
+						) : (
+							<Tooltip title="Your agency is pending approval">
+								<ClockCircleTwoTone style={{ marginLeft: '.5em' }} />
+							</Tooltip>
+						)}
+					</Descriptions.Item>
+					<Descriptions.Item label="Agency type">
+						{agencyTypes[agency.type]}
+					</Descriptions.Item>
+				</Descriptions>
+			)}
 		</>
 	);
 }
@@ -54,16 +58,16 @@ AccountView.propTypes = {
 		displayName: PropTypes.string.isRequired,
 		email: PropTypes.string.isRequired,
 		emailVerified: PropTypes.bool,
-	}).isRequired,
+	}),
 	umbrella: PropTypes.shape({
 		name: PropTypes.string.isRequired,
 		id: PropTypes.string,
-	}).isRequired,
+	}),
 	agency: PropTypes.shape({
 		approved: PropTypes.bool.isRequired,
 		name: PropTypes.string.isRequired,
 		type: PropTypes.string.isRequired,
-	}).isRequired,
+	}),
 };
 
 export default AccountView;
