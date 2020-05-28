e5f3242a3a6d2548cd983239f33d4d35a55a5a15
Lauren <2014laurenng@gmail.com>
2020-05-28 13:05:10 -0700

    trying to avoid merge conflicts

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-<<<<<<< HEAD
 import Navbar from './Site/Navbar';
 
 function Layout({ children }) {
@@ -11,20 +10,6 @@ function Layout({ children }) {
 			<main>{children}</main>
 			Footer
 		</div>
-=======
-import { ThemeProvider } from 'styled-components';
-import { siteTheme } from '../utils/themes';
-
-function Layout({ children }) {
-	return (
-		<ThemeProvider theme={siteTheme}>
-			<div className="site">
-				Header
-				<main>{children}</main>
-				Footer
-			</div>
-		</ThemeProvider>
->>>>>>> abcccc8d2b55c9df344fe90c40691cdfa8be8059
 	);
 }
 
--- src/components/Site/Navbar.js
+++ src/components/Site/Navbar.js
@@ -2,37 +2,46 @@
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby'
 import styled from 'styled-components';
+import '../../templates/page';
 
 const navbarInner = styled('div')`
 	max-width: ${props => props.theme.breakpoints.lg};
 	width: 100%;
+	text-align: center;
 	margin-left: auto;
 	margin-right: auto;
 `;
 
+
 const NavbarNav = styled('nav')`
-	height: auto;
 	background-color: ${props => props.theme.colors.background};
+	height: auto;
 `;
 
-const navbarLink = styled('h2')`
-	font-size: 16px;
-	padding: 1em 0.5em;
+
+const navbarLink = styled.a`
 	color: ${props => props.theme.colors.accent};
+	font-size: 16px;
+	color: pink;
+	padding: 1em 0.5em;
 `;
+
 
 function Navbar() {
 	const data = useStaticQuery(graphql`
-	query MyQuery {
-		allWordpressPage {
-		  edges {
-			node {
-			  slug
-			  title
+			query MyQuery {
+				allWordpressPage {
+					edges {
+						node {
+							slug
+							title
+						}
+					}
+				}
 			}
-		  }
-		}
-	  }`, 'menu_order') // is this right??
+		`,
+		'menu_order'
+	); // is this right??
 
 	// console.log("This data " + data.allWordpressPage.edges);
 	// let thisss = data.allWordpressPage.edges[0];
@@ -40,17 +49,12 @@ function Navbar() {
 	let navbar = data.allWordpressPage.edges;
 
 	let nav = navbar.map((item) => {
-		return(
-			<navbarLink>{item.node.title}</navbarLink>
-		)
-
+		return <navbarLink key={item.node.title}>{item.node.title}</navbarLink>;
 	});
 
 	return (
 		<NavbarNav>
-			<navbarInner>
-				{nav}
-			</navbarInner>
+			<navbarInner>{nav}</navbarInner>
 		</NavbarNav>
 	);
 }
